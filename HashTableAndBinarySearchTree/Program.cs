74144e4bd5bb4e7ac2cdd995348355b48f6f491c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-19 00:35:28 +0530

    [Ritvik]Add: Welcome Message

---
+++ HashTableAndBinarySearchTree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTableAndBinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table And Binary Saerch Tree!!!");
+        }
+    }
+}
