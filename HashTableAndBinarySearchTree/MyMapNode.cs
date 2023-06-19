88139e67492aa154603c032aea6ffc37de2e175b
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-19 18:40:20 +0530

    [Ritvik]Add: Finding the frequency of word in sentence

---
+++ HashTableAndBinarySearchTree/MyMapNode.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBinarySearchTree
+{
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValuePair<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValuePair<K, V>>[size];
+        }
+
+        public struct KeyValuePair<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<K, V>> linkedList = GetLinkedList(position);
+            KeyValuePair<K, V> item = new KeyValuePair<K, V>() { Key = key ,Value = value};
+            linkedList.AddLast(item);
+        }
+
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<K, V>> linkedlist = GetLinkedList(position);
+            bool itemfound = false;
+            KeyValuePair<K, V> founditem = default(KeyValuePair<K, V>);
+            foreach (KeyValuePair<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemfound = true;
+                    founditem = item;
+                }
+            }
+            if (itemfound)
+            {
+                linkedlist.Remove(founditem);
+            }
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<K, V>> linkedlist = GetLinkedList(position);
+            foreach(KeyValuePair<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            var hashcode = key.GetHashCode();
+            int position = hashcode % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValuePair<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValuePair<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValuePair<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public List<KeyValuePair<K, V>> GetAllItems()
+        {
+            List<KeyValuePair<K, V>> allItems = new List<KeyValuePair<K, V>>();
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                {
+                    foreach (var item in linkedList)
+                    {
+                        allItems.Add(item);
+                    }
+                }
+            }
+            return allItems;
+        }
+    }
+}
--- HashTableAndBinarySearchTree/Program.cs
+++ HashTableAndBinarySearchTree/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash Table And Binary Saerch Tree!!!");
+            Console.WriteLine("Welcome to Hash Table And Binary Saerch Tree!!!\n");
+
+            //MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
+            //hash.Add("0", "to");
+            //hash.Add("1", "be");
+            //hash.Add("2", "or");
+            //hash.Add("3", "not");
+            //hash.Add("4", "to");
+            //hash.Add("5", "be");
+            //string hash5 = hash.Get("5");
+            //Console.WriteLine("5th index value is: " + hash5);
+
+            string obj = "to be or not to be";
+
+            string[] words = obj.Split(' ');
+
+            MyMapNode<string, int> wordFrequencyMap = new MyMapNode<string, int>(words.Length);
+
+            foreach (string word in words)
+            {
+                int frequency = wordFrequencyMap.Get(word);
+                wordFrequencyMap.Add(word, frequency + 1);
+            }
+            foreach (var item in wordFrequencyMap.GetAllItems())
+            {
+                Console.WriteLine($"Word: {item.Key}, Frequency: {item.Value}");
+            }
         }
     }
 }
